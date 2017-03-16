78d69eb0d45b2d3c7575d07df477ff6d7433bd56
YourtionGuo <yourtion@gmail.com>
2017-03-16 18:47:27 +0800

    update file

--- src/api.js
+++ src/api.js
@@ -1,8 +1,5 @@
 'use strict';
-
-const fs = require('fs');
-var http = require('http');
-const path = require('path');
+const handler = require('./handler.js');
 
 module.exports = function (router) {
 
@@ -15,41 +12,7 @@ module.exports = function (router) {
   });
 
   router.post('/handler', function (req, res) {
-    console.log(req.file);
-    const cmd = req.query.cmd;
-    const url = req.query.url;
-    const file = req.file;
-    if(!cmd) return res.error('cmd');
-    if(!file && !url) return res.error('file');
-    const command = cmd.split('/');
-    if(command[1] && command[1] === 'info') {
-      const stat = fs.statSync(file);
-      const total = stat.size;
-      return res.success({cmd, file, url, total});
-    }
-    if(url) {
-      const filename = Math.random().toString(36).substr(2);
-      const filepath = path.resolve('/tmp/', filename);
-      const video = fs.createWriteStream(filepath);
-      video.on('finish', () => {
-        video.close(() => {
-          const stat = fs.statSync(filepath);
-          const total = stat.size;
-          res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-          fs.createReadStream(filepath).pipe(res);
-        });
-      });
-      const request = http.get(url, (response) => {
-        response.pipe(video);
-        console.log(url);
-      });
-    } else {
-      const stat = fs.statSync(file);
-      const total = stat.size;
-      res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-      fs.createReadStream(file).pipe(res);
-    }
-
+    handler(req, res);
   });
 
 };
---
+++ src/handler.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+var http = require('http');
+const path = require('path');
+
+module.exports = function(req, res) {
+  const cmd = req.query.cmd;
+  const url = req.query.url;
+  const file = req.file;
+  if(!cmd) return res.error('cmd');
+  if(!file && !url) return res.error('file');
+  const command = cmd.split('/');
+
+  if(command[1] && command[1] === 'info') {
+    const stat = fs.statSync(file);
+    const total = stat.size;
+    return res.success({cmd, file, url, total});
+  }
+  
+  if(url) {
+    const filename = Math.random().toString(36).substr(2);
+    const filepath = path.resolve('/tmp/', filename);
+    const video = fs.createWriteStream(filepath);
+    video.on('finish', () => {
+      video.close(() => {
+        const stat = fs.statSync(filepath);
+        const total = stat.size;
+        res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+        fs.createReadStream(filepath).pipe(res);
+      });
+    });
+    const request = http.get(url, (response) => {
+      response.pipe(video);
+      console.log(url);
+    });
+  } else {
+    const stat = fs.statSync(file);
+    const total = stat.size;
+    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+    fs.createReadStream(file).pipe(res);
+  }
+};

d25d7a342a09cdd89346ba2506af480a72067c60
YourtionGuo <yourtion@gmail.com>
2017-03-16 18:19:57 +0800

    add url download

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+var http = require('http');
 
 module.exports = function (router) {
 
@@ -19,10 +20,28 @@ module.exports = function (router) {
     const file = req.file;
     if(!cmd) return res.error('cmd');
     if(!file && !url) return res.error('file');
-    var stat = fs.statSync(file);
-    var total = stat.size;
-    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-    fs.createReadStream(file).pipe(res);
+    if(url) {
+      const filename = Math.random().toString(36).substr(2);
+      const video = path.resolve('/tmp/', filename)
+      const request = http.get(url, (response) => {
+        response.pipe(video);
+        video.on('finish', () => {
+          video.close(() => {
+            const stat = fs.statSync(video);
+            const total = stat.size;
+            res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+            fs.createReadStream(video).pipe(res);
+          });
+        });
+      });
+      
+    } else {
+      const stat = fs.statSync(file);
+      const total = stat.size;
+      res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+      fs.createReadStream(file).pipe(res);
+    }
+
   });
 
 };
